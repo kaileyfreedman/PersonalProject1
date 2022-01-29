25e820d84db4654e6ac2779da52a4765fb33cc1c
kaileyfreedman <kfreedman2023@chadwickschool.org>
2022-01-29 14:42:00 -0800

    worked on method tests

---
+++ TestGetResult.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PersonalProject1
+{
+    class TestGetResult
+    {
+        public static bool RunTest()
+        {
+            return false;
+        }        
+    }
+}
