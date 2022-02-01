8a5bfd038c3b23617f0c0348d064fe6dfd8658b0
Joseph Collard <jcollard@chadwickschool.org>
2022-02-01 08:20:17 -0800

    chore: Add feedback.

--- TestAskQuestion.cs
+++ TestAskQuestion.cs
@@ -6,13 +6,42 @@ namespace PersonalProject1
     {
         public static bool RunTest()
         {
-            List<string> scores = Program.LoadFile("test_socres.txt").ToList();
+            // Feedback(jcollard: 2022-02-01): You shouldn't need this section here. This was part of my example.
+                // List<string> scores = Program.LoadFile("test_socres.txt").ToList();
 
-            if(scores.Count != 3)
-            {
-                Console.Error.WriteLine("The file test_scores should have 3 lines but that was not the case.");
-                return false;
-            }
+                // if(scores.Count != 3)
+                // {
+                //     Console.Error.WriteLine("The file test_scores should have 3 lines but that was not the case.");
+                //     return false;
+            // }
+
+
+            // TODO(jcollard 2022-02-01): These look great.
+            // Now, figure out how to call your methods. In this case you would do the following:
+            //
+            // 1. Start by setting up a Question object
+            // We do this because your method AskQuestion takes 1 parameter which must be a Question
+            //
+            // Question testQuestion = new Question();
+            // testQuestion.question = "Some question here";
+            // testQuestion.answers.Add("Some answer");
+            // testQuestion.answers.Add("Some other8 answer");
+            //
+            // Then, create a message to be displayed to the tester. What should the method
+            // write to the console when you pass testQuestion as an argument?
+            //
+            // Console.WriteLine("Expected: the output to contain 3 options.");
+            // Console.WriteLine("First type 4. This should be an invalid option. It will ask again.");
+            // Console.WriteLine("Next type 1. This should be a valid option. By selecting 1, the method should return 1.")
+            //
+            // int result = Program.AskQuestion(testQuestion);
+            //
+            // Then we check that the result is equal to 1 because that is what we told the tester to enter.
+            // 
+            // Think of a few more tests you could do. What should happen if the question has no answers?
+            // What should happen if the question has more than 3 answers?
+
+            return false;
         }
     }
 }
